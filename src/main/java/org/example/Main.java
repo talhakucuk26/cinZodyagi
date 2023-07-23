f436463e32690b24f374c4e7599d89ed80131f7e
Talha <talhakucuk26@gmail.comgit config --global user.name Talhagit commitgit config --global user.email talhakucuk26@gmail.com>
2023-07-23 22:23:23 +0300

    Çin Zodyağı Hesaplama

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int date;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Doğum Yılınızı Giriniz : ");
+        date = scanner.nextInt();
+
+        if (date %12 == 0){
+            System.out.println("Maymun");
+        } else if (date %12 == 1) {
+            System.out.println("Horoz");
+        } else if (date %12 == 2) {
+            System.out.println("Köpek");
+        } else if (date %12 == 3) {
+            System.out.println("Domuz");
+        } else if (date %12 == 4) {
+            System.out.println("Fare");
+        } else if (date %12 == 5) {
+            System.out.println("Öküz");
+        } else if (date %12 == 6) {
+            System.out.println("Kaplan");
+        } else if (date %12 == 7) {
+            System.out.println("Tavşan");
+        } else if (date %12 == 8) {
+            System.out.println("Ejderha");
+        } else if (date %12 == 9) {
+            System.out.println("Yılan");
+        }else if (date %12 == 10) {
+            System.out.println("At");
+        }else {
+            System.out.println("Koyun");
+        }
+
+    }
+    }
